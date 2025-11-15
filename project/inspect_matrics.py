1a730886399988473fe8559d27d65bc6458faa30
Abhishek0628 <abhijnv124@gmail.com>
2025-11-15 11:09:27 +0530

    changes made

--- project/inspect_matrics.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-with open("experiments/66/train_metrics.pkl", "rb") as f:
-    data = pickle.load(f)
-
-print("Available keys:", data.keys())
-print("First 10 epochs:", data["epoch"][:10])
-print("First 10 costs:", data["cost"][:10])
--- project/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python -m src.trainer --epochs 120 --num_ants 24 --num_colonies 6 --seed 42
-python -m src.eval --model experiments/best_model.pth --out_dir out
